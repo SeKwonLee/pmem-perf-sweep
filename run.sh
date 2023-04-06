d28f8d86870aae62bc7e49fb3654902373de22ea
Sekwon Lee <sklee@cs.utexas.edu>
2023-04-06 05:42:12 +0000

    Update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo ./pmem_perf_sweep.sh -m ~/projects/mlc/Linux/mlc -p /mnt/pmem1 -s 0 | tee -a PMem_PnP_numa1_single_result.log
+sudo ./pmem_perf_sweep.sh -m ./mlc/Linux/mlc -p /mnt/pmem1 -s 0 | tee -a PMem_PnP_numa1_single_result.log
